1f2a9924786ea0d27016e3d3ecc624aac29829d6
priyanka-kale <priyanka-1994@gmail.com>
2025-06-01 08:08:44 +0530

    Adding Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# base image
+FROM python:alpine
+# set the working directory
+WORKDIR app
+# copy applicaton files
+COPY . /app 
+# Install dependencies
+RUN pip install -r requirements.txt
+# expose the Flask port
+EXPOSE 5000
+# run the flask app
+CMD ["python","run.py"]
+
--- market/models.py
+++ market/models.py
@@ -39,6 +39,12 @@ class User(db.Model, UserMixin):
 
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+    
+    def can_purchase(self,item_obj):
+        return self.budget >= item_obj.price
+    
+    def can_sell(self,item_obj):
+        return item_obj in self.items
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -47,5 +53,16 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    
     def __repr__(self):
         return f'Item {self.name}'
+    
+    def buy(self,user):
+        self.owner = user.id 
+        user.budget -= self.price
+        db.session.commit()
+
+    def sell(self,user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
